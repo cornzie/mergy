7b8d3ee9f780ff3aa02738da619fba8cb89461f4
Cornelius <cornelius.udeh@yahoo.com>
2022-08-24 20:51:31 +0100

    Formats API responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function formattedResponse(mixed $data = [], int $code = 200, string $message = 'success')
+    {
+        return response()->json([
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public function formattedError(string $message = 'Something went wrong', int $code = 500)
+    {
+        return response()->json([
+            'code' => $code,
+            'message' => $message
+        ]);
+    }
 }
