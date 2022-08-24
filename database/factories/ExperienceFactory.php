ad443421d955180fb54c974acdaf4a9c3c581aed
Cornelius <cornelius.udeh@yahoo.com>
2022-08-24 20:52:59 +0100

    Adds and updates fatories

---
+++ database/factories/ExperienceFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Experience>
+ */
+class ExperienceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $user = User::factory()->create();
+        return [
+            'user_id' => $user->id,
+            'job_title' => fake()->sentence(),
+            'location' => fake()->address(),
+            'start_date' => fake()->date(),
+            'end_date' => fake()->date(),
+        ];
+    }
+
+    
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,10 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->safeEmail(),
+            '_id' => 'ahd123@zod',
+            'job' => 'userjob',
+            'cv' => 'http://exampleserver.test/cv.docx',
+            'user_image' => 'http://exampleserver.test/user_image.png',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
