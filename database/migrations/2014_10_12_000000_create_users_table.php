b7bef83cbc7e62983a1e2362bb8141c34bb26028
Cornelius <cornelius.udeh@yahoo.com>
2022-08-24 20:53:14 +0100

    Updates migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('_id')->unique()->index();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('job');
+            $table->string('cv');
+            $table->string('user_image');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
